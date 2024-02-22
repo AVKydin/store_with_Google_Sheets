bd573e3df22516e24829c30280bee10acadbe001
AVKydin <tlalok@ukr.net>
2024-02-22 12:04:15 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+gapi.load('client', initClient);
+
+function initClient() {
+    gapi.client.init({
+        apiKey: 'AIzaSyDtZJ2OspEUtqkCoQvZ5nIrm256zvMYs5I',
+        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    }).then(function() {
+        console.log('Google Sheets API initialized');
+        getDataFromSheet();
+    });
+}
+
+function getDataFromSheet() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1HJzKapn438dVT3vws2Ea7zG9FMmoKv8yE3GbQYvS6GU',
+        range: 'Nike Lebron, Jordan',
+    }).then(function(response) {
+        let values = response.result.values;
+        console.log('Data from Google Sheet:', values);
+    }).catch(function(error) {
+        console.error('Error fetching data:', error.result.error.message);
+    });
+
+}
