78944c8b3ed3cd80d3f1daea25210a0276533a7c
AVKydin <tlalok@ukr.net>
2024-02-24 15:13:53 +0200

    first step

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-gapi.load('client', initClient);
+
+const spreadsheetId = '1HJzKapn438dVT3vws2Ea7zG9FMmoKv8yE3GbQYvS6GU';
+const range = 'Nike Lebron, Jordan';
+const apiKey = 'AIzaSyDtZJ2OspEUtqkCoQvZ5nIrm256zvMYs5I';
+
+gapi.load('client', function() {
+    initClient();
+});
 
 function initClient() {
     gapi.client.init({
-        apiKey: 'AIzaSyDtZJ2OspEUtqkCoQvZ5nIrm256zvMYs5I',
-        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-    }).then(function() {
+        apiKey: apiKey,
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(() => {
         console.log('Google Sheets API initialized');
         getDataFromSheet();
+    }, (error) => {
+        console.error('Error initializing Google Sheets API:', error);
     });
 }
 
 function getDataFromSheet() {
     gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: '1HJzKapn438dVT3vws2Ea7zG9FMmoKv8yE3GbQYvS6GU',
-        // range: 'Nike Lebron, Jordan',
-        range: 'Adidas Yeezy 700',
-    }).then(function(response) {
-        let values = response.result.values;
+        spreadsheetId: spreadsheetId,
+        range: range,
+    }).then((response) => {
+        const values = response.result.values;
         console.log('Data from Google Sheet:', values);
-    }).catch(function(error) {
+        displayData(values);
+    }).catch((error) => {
         console.error('Error fetching data:', error.result.error.message);
     });
-
 }
 
+function displayData(data) {
+    const list = document.getElementById('data-list');
+    list.innerHTML = ''; // Очистити існуючі пункти списку
+
+    data.forEach((row) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = row.join(', ');
+        list.appendChild(listItem);
+    });
+}
