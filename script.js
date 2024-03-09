5a231bbed6889ae46f5710ade36b63217710fa26
AVKydin <tlalok@ukr.net>
2024-03-09 20:58:05 +0200

    add button "up"

--- script.js
+++ script.js
@@ -233,6 +233,26 @@ function showCart(){
     div.appendChild(buttonCart)
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    window.onscroll = function () {
+        scrollFunction();
+    };
+});
+
+function scrollFunction() {
+    let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
 
 
 
